8d978beb0e88e99ec6ab7ddd596b8c370609dfc7
VicLove25 <vlove@una.edu>
2025-09-04 11:44:04 -0500

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,19 +1,17 @@
 import express from 'express'
-import path from 'path';
-// import { dirname } from 'node:path';
-// import { fileURLToPath } from 'node:url';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const app = express()
 const PORT = process.env.PORT || 3000; 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(join(__dirname, 'public')));
 
-// app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
+app.use(express.json()); 
 
-
-// app.use(express.static(__dirname + 'public'));
 
 
 app.get('/', (req, res) => {
@@ -21,15 +19,47 @@ app.get('/', (req, res) => {
 })
 
 // endpoints...middlewares...apis? 
+
 // send an html file
-
 app.get('/barry', (req, res) => {
-  // res.send('barry. <a href="/">home</a>')
-
-  res.sendFile('barry.html'); 
+ 
+  res.sendFile(join(__dirname, 'public', 'barry.html')) 
 
 })
 
+app.get('/api/barry', (req, res) => {
+  // res.send('barry. <a href="/">home</a>')
+  const myVar = 'Hello from server!';
+  res.json({ myVar });
+})
+
+app.get('/api/query', (req, res) => {
+
+  //console.log("client request with query param:", req.query.name); 
+  const name = req.query.name; 
+  res.json({"message": `Hi, ${name}. How are you?`});
+
+  // receivedData.queries.push(req.query.name || 'Guest');
+  // const name = req.query.name || 'Guest';
+  // res.json({ message: `Hello, ${name} (from query param)` });
+});
+
+app.get('/api/url/:iaddasfsd', (req, res) => {
+
+  console.log("client request with URL param:", req.params.iaddasfsd); 
+  // const name = req.query.name; 
+  // res.json({"message": `Hi, ${name}. How are you?`});
+
+});
+
+
+app.get('/api/body', (req, res) => {
+
+  console.log("client request with POST body:", req.query); 
+  // const name = req.body.name; 
+  // res.json({"message": `Hi, ${name}. How are you?`});
+
+});
 
 
 app.listen(PORT, () => {
